b16a810b518f558961689193f263bc082fb3e4dd
Neski <itsneski@gmail.com>
2021-11-26 00:37:32 -0800

    jet analyze-fees update

--- api/analyze-fees.js
+++ api/analyze-fees.js
@@ -1,5 +1,6 @@
 const constants = require('./constants');
 const config = require('./config');
+const {listFeesSync} = require('../db/utils');
 
 module.exports = {
   printFeeAnalysis(peerName, peerId, localFee, remoteFee, profit = 0) {
@@ -8,6 +9,7 @@ module.exports = {
       let color;
       if (m.importance === constants.feeAnalysis.urgent) color = constants.colorRed;
       if (m.importance === constants.feeAnalysis.warning) color = constants.colorYellow;
+      if (m.importance === constants.feeAnalysis.success) color = constants.colorGreen;
       if (color) console.log(color, m.message);
       else console.log(m.message)
     })
@@ -27,6 +29,7 @@ module.exports = {
     const normal = constants.feeAnalysis.normal;
     const warning = constants.feeAnalysis.warning;
     const urgent = constants.feeAnalysis.urgent;
+    const success = constants.feeAnalysis.success;
 
     const maxPpm = global.testMaxPpm || config.rebalancer.maxAutoPpm || constants.rebalancer.maxAutoPpm;
     const enforceMaxPpm = (global.testEnforceMaxPpm === undefined) ? config.rebalancer.enforceMaxPpm : global.testEnforceMaxPpm;
@@ -35,74 +38,98 @@ module.exports = {
     let optimalMaxPpm;
     let array = [];
 
-    let intro = 'evaluating [outbound] ' + peerName + '. current fees: local { base: ' + localFee.base + ', ppm: ' + localFee.rate + '} remote { base: ' + remoteFee.base + ', ppm: ' + remoteFee.rate + ' }. max ppm: ' + maxPpm + ', max ppm is ';
-    intro += (enforceMaxPpm) ? 'being enforced' : 'not being enforced';
+    // get recent fee changes for the peer
+    const feeHistoryDepth = 240;  // minutes
+    let feeHistory = listFeesSync(peerId, feeHistoryDepth);
+    let feeStats;
+    if (feeHistory && feeHistory.length > 0) {
+      let min;
+      let max;
+      let count = 0;
+      feeHistory.forEach(h => {
+        if (h.ppm > 0) {
+          min = (min) ? Math.min(min, h.ppm) : h.ppm;
+          max = (max) ? Math.max(max, h.ppm) : h.ppm;
+          count++;
+        }
+      })
+      if (count > 0) {
+        feeStats = { count: count, min: min, max: max };
+      }
+    }
 
-    array.push({
-      importance: normal,
-      message: intro
-    })
+    // formulate intro
+    let intro = 'evaluating [outbound] ' + peerName;
+    addMessage(success, intro);
+
+    intro = 'current fees: local { base: ' + localFee.base + ', ppm: ' + localFee.rate + '} remote { base: ' + remoteFee.base + ', ppm: ' + remoteFee.rate + ' }. max ppm: ' + maxPpm + ' (max ppm is ';
+    intro += (enforceMaxPpm) ? 'being enforced)' : 'not being enforced)';
+    if (feeStats) intro += '\nthe peer changed their ppm ' + feeStats.count + ' times over the past ' + feeHistoryDepth/60 + ' hours, min: ' + feeStats.min + ', max: ' + feeStats.max;
+    addMessage(normal, intro);
 
     // calculate optimal max ppm
     let optimal = maxPpm;
     if (enforceMaxPpm) {
       if (local > maxPpm) {
-        array.push({
-          importance: normal,
-          message: 'local fee exceeds the max ppm of ' + maxPpm + ', assuming the current max ppm as the optimal max ppm'
-        })
-      } else {
+        addMessage(normal, 'local fee exceeds the max ppm of ' + maxPpm + ', assuming the current max ppm as the optimal max ppm');
+        if (maxPpm < remote) {
+          addMessage(normal, 'remote fee exceeds the max ppm, this means that rebalances can not go through this peer');
+          // check fee stats, at least two occurances, otherwise it don't matter as much
+          if (feeStats && feeStats.count >= 2) {
+            if (feeStats.min < maxPpm) {
+              // the peer has been flip flopping between fees, with the
+              // min being less than the max ppm, so there is a chance
+              // the peer will lower fees
+              addMessage(normal, 'the peers has been changing their fees fairly often, with the min of ' + feeStats.min + ' that is less than the max ppm');
+              addMessage(normal, 'suggestion: keep on monitoring peer\'s fees, there is a chance the peer will drop their fees');
+            } else {
+              // is there a hope for the peer to drop their fees?
+              addMessage(normal, 'suggestion: keep on monitoring peer\'s fees');
+            }
+          } else {  // no fee history, is there hope?
+            addMessage(normal, 'suggestion: keep on monitoring peer\'s fees');
+          }
+          return array;          
+        }
+      } else {  // local <= max ppm
         optimal = local;
-        array.push({
-          importance: normal,
-          message: 'local ppm of ' + localFee.rate + ' is below the max ppm, assuming the local ppm as optimal max ppm so that rebalances are more cost-effective'
-        })
+        addMessage(normal, 'local ppm of ' + localFee.rate + ' is below the max ppm, assuming the local ppm as optimal max ppm so that rebalances are more cost-effective');
       }
+      
       if (optimal < remote) {
-        array.push({
-          importance: urgent,
-          message: 'remote fee exceeds the optimal max ppm of ' + optimal + ', the rebalancer will pause. keep on monitoring peer\'s fees.'
-        })
-        return array; // serious enough to exit
+        if (optimal + buffer < maxPpm) {
+          // this will change once profitability is enforced
+          optimal = optimal + buffer;
+          addMessage(normal, 'adjusting the optimal max ppm to ' + optimal + ' to account for min rebalance buffer');
+        } else {
+          addMessage(urgent, 'remote fee exceeds the optimal max ppm of ' + optimal + ', the rebalancer will pause for this peer');
+          addMessage(normal, 'suggestion: keep on monitoring peer\'s fees');
+          return array; // serious enough to exit
+        }
       }
+
       // let's figure out profit margin
       // make sure we are above the min to be profitable
       if (profit) {
         let minProfitable = Math.round((remote + buffer) * (1 + profit/100));
         let profitOptimal = Math.round(optimal * (1 - profit / 100));
-        array.push({
-          importance: normal,
-          message: 'optimal rebalance max ppm given the profit margin of ' + profit + '% is ' + minProfitable
-        })
+        addMessage(normal, 'optimal rebalance max ppm given the profit margin of ' + profit + '% is ' + minProfitable);
         if (profitOptimal < minProfitable) {
-          array.push({
-            importance: warning,
-            message: 'not enough of a buffer for rebalancer to meet the profitability of ' + profit + '%. consider increasing the local fee to ' + minProfitable
-          })
+          addMessage(warning, 'not enough of a buffer for rebalancer to meet the profitability of ' + profit + '%. consider increasing local ppm to ' + minProfitable);
         } else {
-          array.push({
-            importance: normal,
-            message: 'the rebalance max ppm of ' + profitOptimal + ' meets the profitability of ' + profit + '%'
-          })
+          addMessage(normal, 'the rebalance max ppm of ' + profitOptimal + ' meets the profitability of ' + profit + '%');
         }
       } else {  // profit requirements not specified
         if (optimal < remote) {
-          array.push({
-            importance: urgent,
-            message: 'the optimal max ppm is below the remote fee of ' + remote + '. the rebalancer will pause. keep on monitoring peer\'s fees'
-          })
+          addMessage(urgent, 'the optimal max ppm is below the remote fee of ' + remote + '. the rebalancer will pause for this peer');
+          addMessage(normal, 'suggestion: keep on monitoring peer\'s fees');
         } else if (optimal < remote + buffer) {
-          let msg = 'not enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. this means that rebalances have less of a change to go through.';
-          msg += (optimal === maxPpm) ? ' consider increasing maxPpm to ' + (remote + buffer) : ' consider increasing your local fee to ' + (remote + buffer);
-          array.push({
-            importance: warning,
-            message: msg 
-          })
+          let msg = 'not enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. this means that rebalances have less of a chance to go through';
+          addMessage(normal, msg);
+          msg = (optimal === maxPpm) ? 'suggestion: consider increasing maxPpm to ' + (remote + buffer) : 'suggestion: consider increasing your local ppm to ' + (remote + buffer);
+          addMessage(warning, msg);
         } else {
-          array.push({
-            importance: normal,
-            message: 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. the rebalance is good to go'
-          })
+          addMessage(normal, 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. the rebalance is good to go');
         }
       }
       return array;
@@ -112,50 +139,40 @@ module.exports = {
     let xx = Math.floor(local / maxPpm);
     if (xx >= 10) {
       // the local ppm is insane
-      array.push({
-        importance: urgent,
-        message: 'local fee exceeds the max ppm by more than ' + xx + 'x. revisit your fees. the rebalancer will pause'
-      })
+      addMessage(urgent, 'local fee exceeds the max ppm by more than ' + xx + 'x');
+      addMessage(normal, 'suggestion: revisit your fees. the rebalancer will pause for this peer');
       return array; // important enough to exit
     }
     if (xx >= 2) {
-      array.push({
-        importance: warning,
-        message: 'local fee exceeds the max ppm by more than ' + xx + 'x. consider revisiting your fees'
-      })
+      addMessage(warning, 'local fee exceeds the max ppm by more than ' + xx + 'x');
+      addMessage(normal, 'suggestion: revisit your fees');
     }
     optimal = local;
     if (profit) {
       let minProfitable = Math.round((remote + buffer) / (1 - profit/100));
       let profitOptimal = Math.round(optimal * (1 - profit / 100));
-      array.push({
-        importance: normal,
-        message: 'optimal rebalance max ppm given the profit margin of ' + profit + '% is ' + minProfitable
-      })
+      addMessage(normal, 'optimal rebalance max ppm given the profit margin of ' + profit + '% is ' + minProfitable);
       if (profitOptimal < minProfitable) {
-        array.push({
-          importance: warning,
-          message: 'not enough of buffer for rebalancer to meet the profitability requirements. consider increasing local ppm from ' + localFee.rate + ' to ' + minProfitable
-        })
+        addMessage(warning, 'not enough of buffer for rebalancer to meet the profitability requirements');
+        addMessage(normal, 'suggestion: consider increasing local ppm from ' + localFee.rate + ' to ' + minProfitable);
       } else {
-        array.push({
-          importance: normal,
-          message: 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + profitOptimal + '. the rebalance is good to go'
-        })
+        addMessage(normal, 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + profitOptimal + '. the rebalance is good to go');
       }
     } else {
-      if (optimal < remote + buffer) {
-        array.push({
-          importance: urgent,
-          message: 'not enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. consider increasing local ppm from ' + localFee.rate + ' to ' + (remote + buffer)
-        })
+      if (optimal < remote) {
+        addMessage(urgent, 'the optimal max ppm of ' + optimal + ' is below the remote fee. this means that rebalances will not go through for this peer');
+        addMessage(normal, 'suggestion: keep on monitoring peer\'s fees');
+      } else if (optimal < remote + buffer) {
+        addMessage(warning, 'not enough of a buffer between the remote fee and the optimal max ppm of ' + optimal);
+        addMessage(normal, 'suggestion: consider increasing local ppm from ' + localFee.rate + ' to ' + (remote + buffer));
       } else {
-        array.push({
-          importance: normal,
-          message: 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. the rebalance is good to go'
-        })
+        addMessage(normal, 'there is enough of a buffer between the remote fee and the optimal max ppm of ' + optimal + '. the rebalance is good to go');
       }
     }
     return array;
+
+    function addMessage(importance, message) {
+      array.push({importance:importance, message:message})
+    }
   }
 }
--- test/db.js
+++ test/db.js
@@ -1,7 +1,19 @@
 const dbUtils = require('../db/utils');
+const fs = require('fs');
+const file = '/tmp/jet_test.db';
+if (fs.existsSync(file)) fs.unlinkSync(file);
 dbUtils.enableTestMode();
 
 const timeout = 2 * 1000; // msec
+
+return testHtlcs();
+
+const node = 'nnnnnnnn';
+const chan = 'cccccccc';
+dbUtils.recordFee({node:node, chan:chan, base:100});
+dbUtils.recordFee({node:node, chan:chan, ppm:999});
+dbUtils.recordFee({node:node, chan:chan, base:750, ppm:1999});
+console.log(dbUtils.listFeesSync(node));
 
 dbUtils.recordTelegramMessageSync('hey there');
 dbUtils.recordTelegramMessageSync('another hey there');
@@ -15,14 +27,6 @@ console.log('deleted telegram messages');
 setTimeout(() => {
   console.log('telegram messages:', dbUtils.fetchTelegramMessageSync());  
 }, timeout);
-
-return;
-
-const node = 'aaaaaaaa';
-dbUtils.recordFee({node:node, base:100});
-dbUtils.recordFee({node:node, ppm:999});
-dbUtils.recordFee({node:node, base:750, ppm:1999});
-console.log(dbUtils.listFees(node));
 
 console.log('doesnotexist:', dbUtils.getPropSync('doesnotexist'));
 dbUtils.setPropSync('botChatId', '1234');
@@ -38,50 +42,103 @@ setTimeout(() => {
   console.log(dbUtils.listRebalanceAvoidSync('node_a', 'node_b', 750)); 
 }, timeout);
 
-dbUtils.recordHtlc({
-  incoming_channel_id: '775688960799080448',
-  outgoing_channel_id: '775647179368169473',
-  incoming_htlc_id: '8730',
-  outgoing_htlc_id: '0',
-  timestamp_ns: '1635784333782439038',
-  event_type: 'FORWARD',
-  link_fail_event: {
-    info: {
-      incoming_timelock: 708016,
-      outgoing_timelock: 707976,
-      incoming_amt_msat: '200004799',
-      outgoing_amt_msat: '200004598'
-    },
-    wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
-    failure_detail: 'INSUFFICIENT_BALANCE',
-    failure_string: 'insufficient bandwidth to route htlc'
-  },
-  event: 'link_fail_event'
-})
+function testHtlcs() {
+  const genTimestamp = () => Date.now() * 1000000 + Math.floor(100000 + Math.random() * 900000);
 
-dbUtils.recordHtlc({
-  incoming_channel_id: '754789443771498496',
-  outgoing_channel_id: '770300254396219392',
-  incoming_htlc_id: '1605',
-  outgoing_htlc_id: '0',
-  timestamp_ns: '1635784474205371242',
-  event_type: 'FORWARD',
-  link_fail_event: {
-    info: {
-      incoming_timelock: 708091,
-      outgoing_timelock: 708051,
-      incoming_amt_msat: '261035380',
-      outgoing_amt_msat: '260989706'
+  dbUtils.recordHtlc({
+    incoming_channel_id: '775688960799080448',
+    outgoing_channel_id: '770300254396219392',
+    incoming_htlc_id: '8730',
+    outgoing_htlc_id: '0',
+    timestamp_ns: genTimestamp() + '',
+    event_type: 'FORWARD',
+    link_fail_event: {
+      info: {
+        incoming_timelock: 708016,
+        outgoing_timelock: 707976,
+        incoming_amt_msat: '200004799',
+        outgoing_amt_msat: '200004598'
+      },
+      wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
+      failure_detail: 'INSUFFICIENT_BALANCE',
+      failure_string: 'insufficient bandwidth to route htlc'
     },
-    wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
-    failure_detail: 'INSUFFICIENT_BALANCE',
-    failure_string: 'insufficient bandwidth to route htlc'
-  },
-  event: 'link_fail_event'
-})
-setTimeout(() => {
-  console.log(dbUtils.listHtlcsSync()); 
-}, timeout);
+    event: 'link_fail_event'
+  }, true)
+
+  dbUtils.recordHtlc({
+    incoming_channel_id: '775688960799080448',
+    outgoing_channel_id: '775647179368169473',
+    incoming_htlc_id: '8730',
+    outgoing_htlc_id: '0',
+    timestamp_ns: genTimestamp() + '',
+    event_type: 'FORWARD',
+    link_fail_event: {
+      info: {
+        incoming_timelock: 708016,
+        outgoing_timelock: 707976,
+        incoming_amt_msat: '200004799',
+        outgoing_amt_msat: '200004598'
+      },
+      wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
+      failure_detail: 'INSUFFICIENT_BALANCE',
+      failure_string: 'insufficient bandwidth to route htlc'
+    },
+    event: 'link_fail_event'
+  }, true)
+
+  dbUtils.recordHtlc({
+    incoming_channel_id: '754789443771498496',
+    outgoing_channel_id: '770300254396219392',
+    incoming_htlc_id: '1605',
+    outgoing_htlc_id: '0',
+    timestamp_ns: genTimestamp() + '',
+    event_type: 'FORWARD',
+    link_fail_event: {
+      info: {
+        incoming_timelock: 708091,
+        outgoing_timelock: 708051,
+        incoming_amt_msat: '261035380',
+        outgoing_amt_msat: '260989706'
+      },
+      wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
+      failure_detail: 'INSUFFICIENT_BALANCE',
+      failure_string: 'insufficient bandwidth to route htlc'
+    },
+    event: 'link_fail_event'
+  }, true)
+
+  dbUtils.recordHtlc({
+    incoming_channel_id: '754789443771498496',
+    outgoing_channel_id: '775647179368169473',
+    incoming_htlc_id: '1605',
+    outgoing_htlc_id: '0',
+    timestamp_ns: genTimestamp() + '',
+    event_type: 'FORWARD',
+    link_fail_event: {
+      info: {
+        incoming_timelock: 708091,
+        outgoing_timelock: 708051,
+        incoming_amt_msat: '261035380',
+        outgoing_amt_msat: '260989706'
+      },
+      wire_failure: 'TEMPORARY_CHANNEL_FAILURE',
+      failure_detail: 'INSUFFICIENT_BALANCE',
+      failure_string: 'insufficient bandwidth to route htlc'
+    },
+    event: 'link_fail_event'
+  }, true)
+
+  setTimeout(() => {
+    const days = 2;
+    console.log('over the past ' + days + ' days');
+    console.log(dbUtils.listHtlcsSync({days:days})); 
+    console.log('fromChan: 754789443771498496');
+    console.log(dbUtils.listHtlcsSync({fromChan:'754789443771498496'})); 
+    console.log('toChan: 770300254396219392');
+    console.log(dbUtils.listHtlcsSync({toChan:'770300254396219392'})); 
+  }, timeout);
+}
 
 dbUtils.recordRebalance('node_a', 'node_b', 1000000, 350000);
 dbUtils.recordRebalance('node_a', 'node_c', 2000000, 550000);
