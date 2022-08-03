bebba5ba364290ca3b7b50b59e9b60a19917c949
Neski <itsneski@gmail.com>
2022-08-03 00:55:05 -0700

    fixes #88: logger service now takes into account serverAddress config variable when connecting to lnd remotely

--- api/ln-rpc.js
+++ api/ln-rpc.js
@@ -3,6 +3,6 @@ const {lnrpc} = require('../lnd-api/connect');
 
 const PROTO = __dirname + '/proto/lightning.proto';
 
-const lnRpc = lnrpc(config.lightningProto || PROTO, config.macaroonPath, config.tlsCertPath);
+const lnRpc = lnrpc(config.lightningProto || PROTO, config.macaroonPath, config.tlsCertPath, config.serverAddress);
 
 module.exports = lnRpc;
