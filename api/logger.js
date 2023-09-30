563d8ea12e562762912fa292aafcede998bd2a74
Neski <itsneski@gmail.com>
2023-09-30 15:08:03 -0700

    #102: added function name and line num

--- api/logger.js
+++ api/logger.js
@@ -33,6 +33,26 @@ module.exports = {
 function log(lvl, args) {
   if (!args || args.length === 0) return;
   let s = (args.length > 1) ? args.join(' ') : args[0];
+
+  // get function name and line number
+  const stack = new Error().stack;
+  const parse = stack.split('\n').slice(2)[1];
+  // get function name
+  let ind1 = parse.indexOf('at');
+  let ind2 = parse.indexOf('(', ind1);
+  const part = parse.substring(ind1 + 2, ind2).trim();
+  const fname = (part.indexOf('Object.') !== 0) ? part : undefined;
+  const arr = parse.split('/');
+  let sub = arr[arr.length - 2] + '/' + arr[arr.length - 1];
+  const ind = sub.indexOf(')');
+  const line = sub.substring(0, ind);
+
+  if (level === 'debug') {
+    s = (fname) ? '[' + fname + ',' + line + '] ' + s : '[' + line + '] ' + s;
+  } else {
+    s = (fname) ? '[' + fname + '] ' + s : s;
+  }
+
   logger.log({ 
     level: lvl,
     message: s
